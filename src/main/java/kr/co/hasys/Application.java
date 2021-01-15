b10a1b0934534e32f8efc40556bb6982084a791d
ian-jo <ian.jo@kakaocorp.com>
2021-01-15 16:41:55 +0900

    chore: 기본 구조 및 lombok 추가

--- src/main/java/kr/co/hasys/Application.java
+++ src/main/java/kr/co/hasys/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestDocsStudyApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringRestDocsStudyApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
