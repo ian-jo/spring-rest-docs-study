4f3443f4f2e2b0eefc3b497c8c0e8de6b5c08a83
ian-jo <ian.jo@kakaocorp.com>
2021-01-15 15:39:16 +0900

    initial project

---
+++ src/main/java/kr/co/hasys/SpringRestDocsStudyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestDocsStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/hasys/SpringRestDocsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.hasys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDocsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
