d55dda87b0457482deef8988b62eba23f597a1f3
ian-jo <ian.jo@kakaocorp.com>
2021-01-18 12:06:23 +0900

    fix: 공통 설정 abstract test 추가

---
+++ src/test/java/kr/co/hasys/AbstractControllerTest.java
@@ -0,0 +1,44 @@
+package kr.co.hasys;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@SpringBootTest
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+public abstract class AbstractControllerTest {
+
+    @Autowired
+    public WebApplicationContext context;
+
+    public RestDocumentationResultHandler document;
+
+    public MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(RestDocumentationContextProvider restDocumentation) {
+        this.document = document("{ClassName}/{methodName}", preprocessResponse(prettyPrint()));
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(restDocumentation).uris()
+                        .withScheme("http")
+                        .withHost("127.0.0.1")
+                        .withPort(8080)
+                )
+                .build();
+    }
+
+}
--- src/test/java/kr/co/hasys/study/shop/ShopControllerTest.java
+++ src/test/java/kr/co/hasys/study/shop/ShopControllerTest.java
@@ -1,20 +1,9 @@
 package kr.co.hasys.study.shop;
 
-import org.junit.jupiter.api.BeforeEach;
+import kr.co.hasys.AbstractControllerTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -22,26 +11,7 @@ import static org.springframework.restdocs.request.RequestDocumentation.paramete
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
-//@AutoConfigureRestDocs(uriHost = "sandbox-hair-app.beauty.devel.kakao.com", uriPort = 80)
-class ShopControllerTest {
-
-    @Autowired
-    private WebApplicationContext context;
-
-    private MockMvc mockMvc;
-
-
-    @BeforeEach
-    public void setup(RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-                .apply(documentationConfiguration(restDocumentation).uris()
-                        .withScheme("http")
-                        .withHost("127.0.0.1")
-                        .withPort(8080))
-                .build();
-    }
+class ShopControllerTest  extends AbstractControllerTest {
 
     @Test
     public void getShop() throws Exception {
@@ -49,7 +19,7 @@ class ShopControllerTest {
         this.mockMvc
                 .perform(get("/shops/{shopId}", shopId))
                 .andExpect(status().isOk())
-                .andDo(document("shop-info",
+                .andDo(document.document(
                         pathParameters(
                                 parameterWithName("shopId").description("매장 id")
                         ),
